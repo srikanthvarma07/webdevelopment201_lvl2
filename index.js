5f894d3f00bc9d828f453aa0f2ceb09445514756
srikanth varma <75570276+srikanthvarma07@users.noreply.github.com>
2022-11-15 22:22:34 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+const portno = require("minimist")(process.argv.slice(2));
+console.log("now listening to port: " ,portno);
+let homeContent = "";
+let projectContent = "";
+let registrationContent="";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(portno);
